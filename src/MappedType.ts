6d69ea213f50a8948d52a751e8ca72c0f8e0e8b9
ANETI-Tunnisia <abdelwaheb_ammar@aneti.tn>
2025-01-15 23:04:29 +0100

    correct an error in MappedType

--- src/MappedType.ts
+++ src/MappedType.ts
@@ -2,4 +2,4 @@ type props="prop1"|"prop2" |"prop3"
 type Mypick<T,Props extends keyof T>={
     [P in Props]:T[P];
 }
-type NewType=Mypick<{color:"green",price:200,name;"pen"},"price">;
+type NewType=Mypick<{color:"green",price:200,name:"pen"},"price">;
